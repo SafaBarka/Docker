7bd55d3d13f0569ac098f01ca8d2fe4d1d0cf2ef
SafaBarka <safa.barka.45876@gmail.com>
2020-09-16 19:01:30 +0100

    nginx + php + mysql (add comments)

--- nginx-server/mysql.sh
+++ nginx-server/mysql.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+#change the debconf frontend to noninteractive using the environnement variable DEBIAN_FRONTEND
 export DEBIAN_FRONTEND="noninteractive"
 
+#insert value(answer to a configuration question) into the debconf database
+#pre-seed the debconf database
 echo "mysql-apt-config mysql-apt-config/select-server select mysql-5.7" | /usr/bin/debconf-set-selections
 
+# -O is used to write the output of curl to a file
 curl -O http://repo.mysql.com/mysql-apt-config_0.8.15-1_all.deb
 
+#dpkg needs wget
 apt-get install wget -y
 
+#install a debian software using dpkg command and the option(-i)
 dpkg -i mysql-apt-config_0.8.15-1_all.deb
 
 apt-get update
