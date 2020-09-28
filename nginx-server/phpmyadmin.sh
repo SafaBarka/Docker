feb22c4de541412c666a34bff50412c099cbc0a4
SafaBarka <safa.barka.45876@gmail.com>
2020-09-28 16:53:20 +0100

    nginx + php + mysql +phpmyadmin + wordpress

--- nginx-server/phpmyadmin.sh
+++ nginx-server/phpmyadmin.sh
@@ -2,6 +2,10 @@
 
 service mysql start 
 
+#-----------------------------------------------------------------
+#phpmyadmin
+#-----------------------------------------------------------------
+
 # < ? . /usr/share/nginx/html/phpmyadmin/sql/create_tables.sql ?
 mysql -u root  < /usr/share/nginx/html/phpmyadmin/sql/create_tables.sql
 
@@ -10,5 +14,17 @@ mysql -u root -e "CREATE USER 'safa'@'localhost' IDENTIFIED BY '123567';"
 
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'safa'@'localhost' IDENTIFIED BY '123567'";
 
-#mysql -u safa -p123567 -e "CREATE DATABASE db_name";
+#------------------------------------------------------------------
+#wordpress
+#------------------------------------------------------------------
 
+#create a basic database configuration which can be used for the Wordpress setup.
+mysql -u root -e "CREATE DATABASE wordpress";
+
+mysql -u root -e "GRANT ALL ON wordpress.* TO 'safa'@'localhost' IDENTIFIED BY '123567'"
+
+#import tables (.sql file) to mysql database
+mysql -u root wordpress < /usr/share/nginx/html/wordpress.sql
+
+#we need to flush the privileges so that the current instance of mysql knows about the rcent changes we've made.
+mysql -u root -e "FLUSH PRIVILEGES";
